208bb4d384adcad1aef8a0dcdb4eacc1c8fdbe49
EduardoAraujoB <ea0123450@gmail.com>
2019-11-18 19:28:12 -0300

    Configuring jest and made first test

---
+++ __tests__/components/TechList.test.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+
+import TechList from "../../src/components/TechList";
+
+describe("TechList component", () => {
+  it("should be able to add a new tech", () => {
+    const { getByText, getByTestId } = render(<TechList />);
+
+    fireEvent.click(getByText("Adicionar"));
+
+    expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
+  });
+});
